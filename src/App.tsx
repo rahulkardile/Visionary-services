d9ba9dcf1d7acfc191bda205af9ffb5c46018417
Rahul kardile <rahulkardile321@gmail.com>
2024-08-20 14:50:34 +0530

    about is updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Suspense } from 'react';
+import { Suspense, lazy } from 'react';
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import Header from './components/Header';
 import Loding from './pages/Loding';
-import About from './pages/about';
+// import About from './pages/About';
 
-// const About = lazy(() => import('./pages/about'));
+const About = lazy(() => import('./pages/About'));
 
 function App() {
 
